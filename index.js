92e83d8876a84aef995644c57cd5f70c3bab18a0
Matthew McClure <matt@mmcc.io>
2015-01-22 17:31:52 -0800

    added ability to require index file and run things

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var schedule = require('node-schedule');
 
+var extensionScraper = require('./lib/extension-scraper');
+
 // Run the extension scraper every 24 hours.
 schedule.scheduleJob('0 0 * * *', function(){
   console.log('Running plugin updater...');
   try {
-    require('./lib/extension-scraper').download();
+    extensionScraper.download();
   } catch (e) {
     console.log(e);
   }
 });
+
+
+// Things we want to export if people actually require this module.
+var ex = {
+  scrape: function() {
+    extensionScraper.download();
+  }
+}
+
+module.exports = ex;
