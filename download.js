f62dfa7a9e2045e3ad103e3873f51dc4fa0b12c8
Matthew McClure <m@mmcc.io>
2015-11-02 13:39:48 -0800

    added ability to specify a custom download location

--- download.js
+++ download.js
@@ -1,4 +1,6 @@
-require('./lib/extension-scraper').download(function(error, results) {
+var fileLocation = process.argv[2] || 'data/extensions.json';
+
+require('./lib/extension-scraper').download({location: fileLocation}, function(error, results) {
   if (error) {
     return console.log('Failed:', error);
   }
